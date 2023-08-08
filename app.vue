45cbf7a17737dbb0fe90006a954c11adbad5a4e1
Tonmoy Pramanik <tonmoy@softzino.com>
2023-08-08 16:47:37 +0600

    upload project

---
+++ app.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <header
+      class="sticky bg-slate-900 top-0 flex justify-between items-center z-50 space-x-1 p-4 shadow-sm"
+    >
+      <a
+        class="font-thin text-3xl text-white"
+        href="#"
+        target="_blank"
+        rel="noopener noreferrer"
+        >CarTreader</a
+      >
+    </header>
+    <!-- car detail page -->
+    <div>
+      <div
+      class="mx-auto mt-4 max-w-7xl space-y-4 px-4 xs:px-8 sm:px-10 lg:px-16 pb-16 w-3/5 font-light"
+      >
+      <div class="mt-10">
+        <img src="https://www.drivespark.com/images/2022-06/2023-toyota-corolla-cross-hybrid-1.jpg" class="w-full" alt="">
+        <h1 class="mt-10 text-4xl">Corolla Cross</h1>
+        <div class="text-slate-500 text-lg border-b pb-5 flex mt-3 justify-between">
+          <div class="flex">
+            <p class="mr-2">5 seat</p>
+            <p class="mr-2">|</p>
+            <p class="mr-2">63,473 miles</p>
+          </div>
+          <div class="font-bold text-2xl">$45,434</div>
+        </div>
+      </div>
+      </div>
+      <div class="mr-10 mt-5 border-b pb-5">
+        <div class="flex text-lg mt-2">
+          <p class="rounded text-lime-800 mr-3">2</p>
+          <p class="rounded text-lime-800 mr-3">Leather Interior</p>
+
+        </div>
+      </div>
+    </div>
+    <!-- car detail page -->
+  </div>
+</template>
